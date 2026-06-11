db7d67a2debf6ef4470af32cafa1146a5be4a18b
j-colours <shrewsburyj2@winthrop.edu>
2026-06-11 03:58:03 -0400

    STR #3: Fixed requirements and venv for correct dependencies and used ai to learn a few common endpoints before resorting to actual documentation

--- main.py
+++ main.py
@@ -4,14 +4,23 @@
 # Date: 2026-06-11
 # Description: ...
 
-from console import console
 import click
 import requests
 import subprocess
+from console import console
 
 
 def main():
-    pass
+    codeberg = requests.get(
+        "https://codeberg.org/api/v1/repos/j-colours/spill-the-repo",
+    ).json()
+
+    codeberg_search = requests.get(
+        "https://codeberg.org/api/v1/repos/search?q=spill-the-repo"
+    ).json()
+
+    console.print(f"[yellow]Username Known Repo: {codeberg['owner']['login']}\n")
+    console.print(f"[red]Search Repo: {codeberg_search['data'][0]['owner']['login']}")
 
 
 if __name__ == "__main__":
