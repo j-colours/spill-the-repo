baadae88ffe7e60dc64f3b294e284f122d2e0c38
j-colours <shrewsburyj2@winthrop.edu>
2026-06-11 03:09:13 -0400

    STR #2: Added current essential files for building tool including requirements and such

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Author: jcolours
+# Date: 2026-06-11
+# Description: ...
+
+from console import console
+import click
+import requests
+import subprocess
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
